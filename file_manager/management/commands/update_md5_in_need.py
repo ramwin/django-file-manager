5e970126580852aa217cc6c82ff8381e4fe663c8
Xiang Wang <ramwin@qq.com>
2023-05-08 23:47:12 +0800

    fix: filter md5 first

--- file_manager/management/commands/update_md5_in_need.py
+++ file_manager/management/commands/update_md5_in_need.py
@@ -23,9 +23,8 @@ class Command(BaseCommand):
                     count__gte=2)
         for info in queryset:
             for f in File.objects.filter(folder=info['folder'],
+                                         md5="",
                                          size=info['size']):
-                if f.md5:
-                    continue
                 self.stdout.write(self.style.HTTP_INFO(f"cal the md5 of {f}"))
                 md5 = get_md5(Path(f.folder.path) / f.path)
                 f.md5 = md5
