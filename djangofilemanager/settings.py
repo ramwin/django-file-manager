745a13df4d31cdc2addc035091e95af121ee7937
Xiang Wang <ramwin@qq.com>
2024-01-05 00:13:06 +0800

    feat: support postgresql

--- djangofilemanager/settings.py
+++ djangofilemanager/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import dotenv_values
 
+
+CONFIG = dotenv_values()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,9 +82,9 @@ WSGI_APPLICATION = 'djangofilemanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': CONFIG.get("DATABASE_ENGINE", 'django.db.backends.sqlite3'),
+        'NAME': CONFIG.get("DATABASE_NAME", BASE_DIR / "db.sqlite3"),
+    },
 }
 
 
